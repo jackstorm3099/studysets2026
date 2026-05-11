9ce4faf4f4ef4e16739dd2aeac3d932451f92bfb
jackstorm3099 <splashywarz@gmail.com>
2026-05-10 19:53:04 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
+let equalPresses = 0;
+let unlockMode = false;
 
+function press(value) {
+  const display = document.getElementById("display");
+
+  if (value === '=') {
+
+    if (display.value === '') {
+      equalPresses++;
+
+      if (equalPresses >= 3) {
+        unlockMode = true;
+        display.placeholder = 'Enter Code';
+      }
+
+      return;
+    }
+
+    try {
+      display.value = eval(display.value);
+    } catch {
+      display.value = 'Error';
+    }
+
+    return;
+  }
+
+  if (unlockMode) {
+    display.value += value;
+
+    if (display.value === '6825') {
+      window.location.href = 'https://sites.google.com/view/pixelatedpix6825/home';
+    }
+
+    return;
+  }
+
+  display.value += value;
+}
+
+function clearDisplay() {
+  document.getElementById('display').value = '';
+}
